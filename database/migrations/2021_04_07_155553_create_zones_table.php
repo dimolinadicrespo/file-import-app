6ec528560de1eec8a7345d4ce2d115e05a5099b4
Diego Molina <molinacrespo@gmail.com>
2021-04-13 11:17:51 +0200

    Migrations

---
+++ database/migrations/2021_04_07_155553_create_zones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateZonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zones', function (Blueprint $table) {
+            $table->id();
+            $table->integer('zone_id');
+            $table->string('zone_name');
+            $table->string('start_date');
+            $table->string('finish_date');
+            $table->integer('days_out_of_period')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zones');
+    }
+}
---
+++ database/migrations/2021_04_10_162851_create_errors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateErrorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('errors', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_chunk');
+            $table->string('number_of_errors');
+            $table->string('line_numbers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('errors');
+    }
+}
