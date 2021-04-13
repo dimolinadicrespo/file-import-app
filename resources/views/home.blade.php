78b3d7fc7e44b234afe1cf0489fc81991e3b759d
Diego Molina <molinacrespo@gmail.com>
2021-04-13 16:34:35 +0200

    Alert info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,12 @@
 @section('content')
     <div class="container">
         <div class="col-md-10 mx-auto">
+            <div class="alert alert-warning" role="alert">
+                <div class="d-flex justify-content-between align-items-center">
+                    Fields in the csv file must be separated by `;`
+                    <i class="fas fa-info-circle"></i>
+                </div>
+            </div>
             <import-file-component></import-file-component>
         </div>
     </div>
