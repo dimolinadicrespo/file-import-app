546ab47b710d69293ba86b319261240601cdc7ea
Diego Molina <molinacrespo@gmail.com>
2021-04-13 11:57:10 +0200

    Seeder for user and zones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('zones')->truncate();
+        DB::table('users')->truncate();
+         User::factory(1)->create([
+             'name' => 'admin',
+             'email' => 'admin@admin.es',
+             'password' => Hash::make('12345678'),
+             'remember_token' => Str::random(10),
+         ]);
     }
 }
